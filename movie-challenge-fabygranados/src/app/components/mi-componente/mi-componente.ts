8246f518da748914b567a02b01a93ebe9481a33e
Fabiola Granados <faby.granados@gmail.com>
2023-10-01 19:23:09 -0700

    Instalaciones y configuracion Angular y  Primeros componentes

---
+++ movie-challenge-fabygranados/src/app/components/mi-componente/mi-componente.ts
@@ -0,0 +1,54 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'mi-componente',
+    templateUrl: './mi-componente.component.html'
+}) 
+export class MiComponente {
+
+    public title: string;
+    public movies: Array<any>;
+    public year: number;
+
+    constructor() {
+        this.title = 'First K-Horror Movie app';
+        this.movies = [
+            {
+                title: 'The Shining',
+                year: 1980
+            },
+            {
+                title: 'The Conjuring',
+                year: 1990
+            },
+            {
+                title: 'The Dark Knight',
+                year: 2008
+            },
+            {
+                title: 'The Dark Knight Rises',
+                year: 2012
+            },
+            {
+                title: 'The Prestige',
+                year: 2006
+            },
+            {
+                title: 'The Matrix',
+                year: 1999
+            },
+            {
+                title: 'The Matrix Reloaded',
+                year: 2003
+            },
+            {
+                title: 'The Matrix Revolutions',
+                year: 2003
+            }
+        ];
+        this.year = new Date().getFullYear();
+    
+        console.log('mi-componente cargado!');
+        console.log(this.movies);
+    }
+}
