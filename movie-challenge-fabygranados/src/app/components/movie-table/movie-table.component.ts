4f7e3f2f17164be6b9675564939c9ad7a15d75e0
Fabiola Granados <faby.granados@gmail.com>
2023-10-10 13:21:03 -0700

    Darle funcionalidad al componente movitables, con la importacion de nuevos modulos de angularMaterial

--- movie-challenge-fabygranados/src/app/components/movie-table/movie-table.component.ts
+++ movie-challenge-fabygranados/src/app/components/movie-table/movie-table.component.ts
@@ -1,13 +1,44 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import {MatSort, MatSortModule} from '@angular/material/sort';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { Movies } from '../data';
+import { Movie } from 'src/app/models/movies.model';
+
 
 @Component({
   selector: 'app-movie-table',
   standalone: true,
-  imports: [CommonModule],
+  imports: [MatPaginatorModule, MatTableModule, MatFormFieldModule, MatInputModule, MatSortModule],
   templateUrl: './movie-table.component.html',
   styleUrls: ['./movie-table.component.scss']
 })
-export class MovieTableComponent {
+export class MovieTableComponent implements OnInit {
+  displayedColumns: string[] = ["title", "poster_path", "release_date" ];
+  dataSource: MatTableDataSource<Movie>;
 
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor() {
+    // Assign the data to the data source for the table to render
+    this.dataSource = new MatTableDataSource(Movies);
+  }
+
+  ngAfterViewInit() {
+    this.dataSource!.paginator = this.paginator;
+    this.dataSource!.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+  ngOnInit() {}
 }
