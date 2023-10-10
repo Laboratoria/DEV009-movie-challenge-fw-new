26e6a2d9205f16e70631bf9118830a7f5560716b
Fabiola Granados <faby.granados@gmail.com>
2023-10-10 13:23:01 -0700

    Implementacion de animaciones de Material
    Se agrego browserAnimationsModule  y se borro browserModule

--- movie-challenge-fabygranados/src/app/app.module.ts
+++ movie-challenge-fabygranados/src/app/app.module.ts
@@ -1,21 +1,16 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MiComponente } from './components/mi-componente/mi-componente';
-import { PrincipalComponent } from './components/principal/principal.component';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MiComponente,
-    PrincipalComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     ToolbarComponent
